aabcbc442484374a148bc6e11746a9ceddcbfff7
Felipe Solórzano <94859615+FelipeJsSM@users.noreply.github.com>
2026-04-10 22:12:08 -0400

    Add project files.

---
+++ BasketManager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BasketManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BasketManager/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BasketManager")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BasketManager.Pages")]
